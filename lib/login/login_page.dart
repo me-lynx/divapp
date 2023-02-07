e45b1785e9282d24385eebd8585d1c7763cab035
Maiza Louise <maizalouise@gmail.com>
2023-02-07 15:55:55 -0300

    Login Page

--- lib/login/login_page.dart
+++ lib/login/login_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -11,73 +13,91 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage("assets/images/woman_home.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Container(
-            width: 300,
-            height: 400,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            child: Column(
-              children: [
-                const SizedBox(height: 50),
-                const Text(
-                  "Login",
-                  style: TextStyle(
-                    fontSize: 24,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const SizedBox(height: 50),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Username",
+      backgroundColor: Colors.black,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            ClipRRect(
+              child: Column(
+                children: [
+                  Container(
+                    width: double.maxFinite,
+                    height: MediaQuery.of(context).size.height,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: _image.image,
+                            fit: BoxFit.cover)), // Background
+                    child: BackdropFilter(
+                      filter: _imageFilter,
+                      child: Padding(
+                        padding: const EdgeInsets.all(30),
+                        child: Center(
+                          child: Container(
+                            width: double.maxFinite,
+                            height: 400,
+                            decoration: BoxDecoration(
+                              color: Colors.white60,
+                              borderRadius: BorderRadius.circular(20),
+                              backgroundBlendMode: BlendMode.srcIn,
+                            ),
+                            child: Column(
+                              children: [
+                                const SizedBox(
+                                  height: 40,
+                                ),
+                                const Text(
+                                  "Div.app",
+                                  style: TextStyle(
+                                    color: Colors.black,
+                                    fontFamily: "Poppins-Black",
+                                    fontSize: 40,
+                                  ),
+                                ),
+                                const SizedBox(height: 50),
+                                Padding(
+                                  padding: const EdgeInsets.all(20.0),
+                                  child: TextFormField(
+                                    decoration: const InputDecoration(
+                                      hintText: "Username",
+                                    ),
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.all(20.0),
+                                  child: TextFormField(
+                                    decoration: const InputDecoration(
+                                      hintText: "Password",
+                                    ),
+                                  ),
+                                ),
+                                Container(
+                                  color: Colors.black,
+                                  child: TextButton(
+                                    child: const Text(
+                                      'LOGIN',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    onPressed: () {},
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: TextFormField(
-                    decoration: const InputDecoration(
-                      hintText: "Password",
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 50),
-                Container(
-                  width: double.infinity,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  // child: FlatButton(
-                  //   onPressed: () {},
-                  //   child: Text(
-                  //     "Login",
-                  //     style: TextStyle(
-                  //       color: Colors.white,
-                  //       fontSize: 18,
-                  //       fontWeight: FontWeight.bold,
-                  //     ),
-                  //   ),
-                  // ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
   }
+
+  Image get _image => Image.asset("assets/images/woman_home.jpg");
+
+  ImageFilter get _imageFilter =>
+      ImageFilter.blur(sigmaX: 8, sigmaY: 8, tileMode: TileMode.repeated);
 }
